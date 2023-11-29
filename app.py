aba3a7b99fb885ce098d9312f6342a250b5a5579
Martin Stickney <mstickney27@saintjosephhs.com>
2023-11-29 08:37:40 -0500

    commit on 11/29/23

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import Weather
 
 app = Flask('weather')
@@ -14,12 +14,12 @@ def index():
     windChill = wx.current_windChill(location)
     visibility = wx.current_visibility(location)
 
-    ret += '\n'
-    ret += f"<p>Your current temperature is {temp['fahrenheit']}F"
-    ret += '\n'
-    ret += f"</br>Wind is blowing at {wind['speed_mph']}mph causing a wind chill of {windChill['windchill_degC']}C"
-    ret += '\n'
-    ret += f"</br>Visibility is {visibility['visibility_m']}m"
-    ret += '</p>'
+    #ret += '\n'
+    #ret += f"<p>Your current temperature is {temp['fahrenheit']}F"
+    #ret += '\n'
+    #ret += f"</br>Wind is blowing at {wind['speed_mph']}mph causing a wind chill of {windChill['windchill_degC']}C"
+    #ret += '\n'
+    #ret += f"</br>Visibility is {visibility['visibility_m']}m"
+    #ret += '</p>'
 
-    return ret
+    return render_template('main.html',temp=temp,wind=wind,windChill=windChill,visibility=visibility)
